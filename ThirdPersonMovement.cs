513bec19a4454dec4967d6addf36fa487ec1c36f
Eijs666 <53994213+Eijs666@users.noreply.github.com>
2021-05-23 15:50:52 +0200

    Update ThirdPersonMovement.cs

--- ThirdPersonMovement.cs
+++ ThirdPersonMovement.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Code was inspired from the following video
+"THIRD PERSON MOVEMENT in Unity"
+https://www.youtube.com/watch?v=4HpC--2iowE
+*/
+
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller; // Declaring our Charactercontroller
